db8a245ade5cd346c9ebc85945d63464cb2ce280
antonijo <a.djodj@gmail.com>
2025-01-13 20:02:25 +0000

    prepare passport

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { RecipesModule } from './recipes/recipes.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [RecipesModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
